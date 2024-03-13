2009ebe7fa370daeecf58e79eb02c486559bb953
theCodemander <es.santigarcia@gmail.com>
2024-03-13 18:04:40 +0100

    Added correct folder structure

---
+++ src/main/java/com/theCodemander/necromantic/item/NecromanticCreativeTab.java
@@ -0,0 +1,14 @@
+package com.theCodemander.necromantic.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class NecromanticCreativeTab {
+    public static final CreativeModeTab NECROMANTIC_MOD_TAB = new CreativeModeTab("necromantic_mod_tab") {
+        @SuppressWarnings("null")
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(NecromanticItems.SOUL_CRYSTAL.get());
+        }
+    };
+}
--- src/main/java/com/theCodemander/necromantic/item/NecromanticItems.java
+++ src/main/java/com/theCodemander/necromantic/item/NecromanticItems.java
@@ -13,9 +13,9 @@ public class NecromanticItems {
     public static final DeferredRegister<Item> ITEMS = 
             DeferredRegister.create(ForgeRegistries.ITEMS, NecromanticMod.MOD_ID);
     
-    //@SuppressWarnings("null")
-    //public static final RegistryObject<Item> SOUL_CRYSTAL = ITEMS.register("soul_crystal", 
-    //    () -> new Item(new Item.Properties().tab(NecromanticCreativeTab.NECROMANTIC_MOD_TAB)));
+    @SuppressWarnings("null")
+    public static final RegistryObject<Item> SOUL_CRYSTAL = ITEMS.register("soul_crystal", 
+        () -> new Item(new Item.Properties().tab(NecromanticCreativeTab.NECROMANTIC_MOD_TAB)));
 
 
     public static void register(IEventBus eventBus){
